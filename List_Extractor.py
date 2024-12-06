816855c5ce973d48ead9f3eb17e3f3ab477d0030
neusv <neus.vegas@autonoma.cat>
2024-12-06 14:43:53 +0100

    Preprocessing file

--- List_Extractor.py
+++ List_Extractor.py
@@ -1,110 +1,88 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 24 17:03:05 2024
-
-@author: atara
-"""
-
-import pandas as pd
-import networkx as nx
 import os
 import pickle
+import networkx as nx
+import pandas as pd
+
 # import matplotlib.pyplot as plt
 
 
-Interactome_file_name = "BIOGRID-ALL-4.4.239.tab3.txt" # starter file
-Seeds_file_name = "DISEASES_Summary_GDA_CURATED_C0025202.tsv"
+interactome_file_name = "data/BIOGRID-ALL-4.4.240.tab3.txt"  # starter file
+seeds_file_name = "data/DISEASES_Summary_GDA_CURATED_C0025202.tsv"
 human_ID = 9606
 interaction_of_interest = "physical"
-Protein_A_identifier = "Entrez Gene Interactor A"
-Protein_B_identifier = "Entrez Gene Interactor B"
-PPI_list_variable = "PPI_of_Interest.pk1"
-PPI_list_variable_second_try = "PPI_of_interest_second_try.pk1"
-First_gene_symbol_indicator = "Official Symbol Interactor A"
-Second_gene_symbol_indicator = "Official Symbol Interactor B"
+protein_A_identifier = "Entrez Gene Interactor A"
+protein_B_identifier = "Entrez Gene Interactor B"
+PPI_list_variable = "data/PPI_of_interest.pk1"
+first_gene_symbol_indicator = "Official Symbol Interactor A"
+second_gene_symbol_indicator = "Official Symbol Interactor B"
 
-#%% Importing and Processing the Interactome
 
-if os.path.exists(PPI_list_variable_second_try):
+# Importing and Processing the Interactome
+
+if os.path.exists(PPI_list_variable):
     print("The file already exists! Importing information from the file ...")
     with open(PPI_list_variable, 'rb') as file:
         PPI = pickle.load(file)
-    
+
 else:
     print("The file does not exist yet. Processing... ")
-    complete_interactome = pd.read_csv(Interactome_file_name, delimiter='\t')
-    
+    complete_interactome = pd.read_csv(interactome_file_name, delimiter='\t')
+
     human_interactome = complete_interactome[
         (complete_interactome["Organism ID Interactor A"] == human_ID)
         & (complete_interactome["Organism ID Interactor B"] == human_ID)]
-    
+
     human_physical_interactome = human_interactome[
         (human_interactome["Experimental System Type"] == interaction_of_interest)]
-    
-    ## human_physical_interactome.to_csv("human_physical_interactome.csv", index = False)
-    
-    # Duplicate Removal
-    
-    # PPI_NoDuplicates = human_physical_interactome.drop_duplicates(
-    #     subset = [Protein_A_identifier, Protein_B_identifier])
-    
-    # PPI_NoDuplicates_NoSelfLoop = PPI_NoDuplicates[
-    #     PPI_NoDuplicates[Protein_A_identifier] != PPI_NoDuplicates[Protein_B_identifier]]
-    
+
     PPI_NoDuplicates = human_physical_interactome.drop_duplicates(
-        subset = [First_gene_symbol_indicator, Second_gene_symbol_indicator])
-    
+        subset=[first_gene_symbol_indicator, second_gene_symbol_indicator])
+
     PPI_NoDuplicates_NoSelfLoop = PPI_NoDuplicates[
-        PPI_NoDuplicates[First_gene_symbol_indicator] 
-        != 
-        PPI_NoDuplicates[Second_gene_symbol_indicator]]
-    # Note that with this second version, 
-    # the number of edges are "slightly" different 
+        PPI_NoDuplicates[first_gene_symbol_indicator]
+        !=
+        PPI_NoDuplicates[second_gene_symbol_indicator]]
+    # Note that with this second version,
+    # the number of edges are "slightly" different
     # (9M with the first version vs 8.7 with the second version)
-    # I think I have to review Database and GeneSymbols 
-    
+    # I think I have to review Database and GeneSymbols
+
     PPI = PPI_NoDuplicates_NoSelfLoop
-    with open(PPI_list_variable_second_try, 'wb') as file:
+    with open(PPI_list_variable, 'wb') as file:
         pickle.dump(PPI, file)
-        
+
     del complete_interactome, human_interactome, human_physical_interactome, \
         PPI_NoDuplicates, PPI_NoDuplicates_NoSelfLoop
 
-
-
-#%% Create the Interactome LCC
-Graph_of_PPI = nx.from_pandas_edgelist(PPI, Protein_A_identifier, Protein_B_identifier)
-print(Graph_of_PPI)
-
+# %% Create the Interactome LCC
+graph_of_PPI = nx.from_pandas_edgelist(PPI, protein_A_identifier, protein_B_identifier)
+print(graph_of_PPI)
 
 ## To verify
-Connected_Components = nx.connected_components(Graph_of_PPI)
- 
-largest_cc= max(Connected_Components, key=len)
+connected_components = nx.connected_components(graph_of_PPI)
 
-LCC_subgraph = nx.subgraph(Graph_of_PPI, largest_cc).copy()
+largest_cc = max(connected_components, key=len)
+
+LCC_subgraph = nx.subgraph(graph_of_PPI, largest_cc).copy()
 print(LCC_subgraph)
 
-
-#%% Importing and the seed genes
-# Note that this will not be the final version, because 
-Seed_genes_df = pd.read_csv(Seeds_file_name, sep = '\t')
+# %% Importing and the seed genes
+# Note that this will not be the final version, because
+Seed_genes_df = pd.read_csv(seeds_file_name, sep='\t')
 Seed_genes_Symbols = Seed_genes_df["Gene"]
 
 Disease_Interactome = PPI[
-    (PPI[First_gene_symbol_indicator].isin(Seed_genes_Symbols)) 
+    (PPI[first_gene_symbol_indicator].isin(Seed_genes_Symbols))
     &
-    (PPI[Second_gene_symbol_indicator].isin(Seed_genes_Symbols))
+    (PPI[second_gene_symbol_indicator].isin(Seed_genes_Symbols))
     ]
 
 Graph_of_disease_interactome = nx.from_pandas_edgelist(
-    Disease_Interactome, 
-    First_gene_symbol_indicator, Second_gene_symbol_indicator)
+    Disease_Interactome,
+    first_gene_symbol_indicator, second_gene_symbol_indicator)
 
 nx.draw(Graph_of_disease_interactome)
-# there are some self-loops, we have to debug the initial list  
+# there are some self-loops, we have to debug the initial list
 # processing of the PPI; maybe the fact that this graph is determined by the
 # official gene symbols could have changed something.
 # Yes, definitely with this new version we don't have self loops no more
-
-
